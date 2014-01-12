cb73fb07e1587927a74fb3d3763dfda8a00278af
Albert <albertkoy@gmail.com>
2014-01-11 23:26:58 -0500

    remove rvm from deployment

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -1,40 +1,29 @@
-require 'rvm/capistrano'
 require 'bundler/capistrano'
 
-#RVM and bundler settings
-set :rvm_ruby_string, :local
+server "173.245.5.162:2200", :web, :app, :db, primary: true
 set :rack_env, :production
 
 #general info
-set :user, 'root'
-set :domain, 'www.umdsocialscheduler.com'
-set :applicationdir, "/home/umd-social-scheduler"
-set :scm, 'git'
 set :application, "umd-social-scheduler"
-set :repository,  "git@github.com:akoy93/umd-social-scheduler.git"
+set :user, 'root'
+set :domain, "www.umdsocialscheduler.com"
+set :use_sudo, false
+
+set :scm, 'git'
+set :repository, "git@github.com:akoy93/umd-social-scheduler.git"
 set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-set :deploy_via, :remote_cache
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+
 #deploy config
-set :deploy_to, applicationdir
-set :deploy_via, :export
+set :deploy_to, "/home/umd-social-scheduler"
+set :deploy_via, :remote_cache
 
 #addition settings. mostly ssh
 ssh_options[:forward_agent] = true
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
-ssh_options[:paranoid] = false
 default_run_options[:pty] = true
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
 # After an initial (cold) deploy, symlink the app and restart nginx
 after "deploy:cold" do
@@ -62,7 +51,6 @@ namespace :deploy do
     deploy.start
   end
 end
-
 
 namespace :admin do
   desc "Restart nginx."
